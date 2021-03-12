73654d6e1040f338b87628b01ee914c5c34f90d0
Gabriel Fernandes Ribeiro <contato.gfernandesribeiro@outlook.com>
2021-03-12 16:34:47 -0300

    Remaking product landing page with bootstrap

---
+++ script.js
@@ -0,0 +1,23 @@
+alert("This is a fictional webpage, made by Gabriel Fernandes Ribeiro (GitHub: https://github.com/gabsschrodinger");
+
+let email = document.querySelector("input#exampleInputEmail1");
+let fullName = document.querySelector("input#exampleInputPassword1");
+
+let okName = false;
+let okEmail = false;
+
+function validateName() {
+    if(fullName.value.length >= 3) okName = true;
+}
+
+function validateEmail() {
+    if(email.value.indexOf("@") !== -1 && email.value.indexOf(".") !== -1) okEmail = true;
+}
+
+function send() {
+    if(okName == true && okEmail == true) {
+        alert("Submitted!");
+    } else {
+        alert("Please, fill the form correctly before submitting");
+    }
+}
